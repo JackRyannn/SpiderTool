f97d336308bfae9c82b54ca61603cc78e94a703f
renchao03 <renchao03@meituan.com>
2018-07-05 17:41:11 +0800

    Merge branch 'rent' of github.com:JackRyannn/SpiderTool into rent
    # Conflicts:
    #	SpiderTool/spiders/rent_spider.py

--- SpiderTool/items.py
+++ SpiderTool/items.py
@@ -12,8 +12,19 @@ class SpidertoolItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field();
-    url = scrapy.Field();
-
+    score = scrapy.Field();
+    star = scrapy.Field();
+    release = scrapy.Field();
+    duration = scrapy.Field();
+    director = scrapy.Field();
+    actors = scrapy.Field();
+    region = scrapy.Field();
+    category = scrapy.Field();
+    enough = scrapy.Field();
+    showed = scrapy.Field();
+    votecount = scrapy.Field();
+    subject = scrapy.Field();
+    pic = scrapy.Field();
     pass
 
 class SpidertoolItem2(scrapy.Item):
--- SpiderTool/pipelines.py
+++ SpiderTool/pipelines.py
@@ -9,7 +9,7 @@ import mysql.connector
 
 class SpidertoolPipeline(object):
     def __init__(self):
-        self.conn = mysql.connector.connect(user='root', password='123456', database='mysql')
+        self.conn = mysql.connector.connect(user='root', password='135213521352', database='mysql')
         self.cursor = self.conn.cursor()
 
 
@@ -17,32 +17,45 @@ class SpidertoolPipeline(object):
     def process_item(self, item, spider):
 
         title = item.get('title')[0]
-        url = item.get('url')[0]
-
+        score = item.get('score')[0]
+        star = item.get('star')[0]
+        release = item.get('release')[0]
+        duration = item.get('duration')[0]
+        director = item.get('director')[0]
+        actors = item.get('actors')[0]
+        region = item.get('region')[0]
+        category = item.get('category')[0]
+        enough = item.get('enough')[0]
+        showed = item.get('showed')[0]
+        votecount = item.get('votecount')[0]
+        subject = item.get('subject')[0]
+        pic = item.get('pic')[0]
+        
+        
         insert_sql = """
-            insert ignore into douban_rent(`title`,`url`)
-            VALUES (%s, %s);
+            insert ignore into douban_movie(`data-title`,`data-score`,`data-star`,`data-release`,`data-duration`,`data-director`,`data-actors`,`data-region`,`data-category`,`data-enough`,`data-showed`,`data-votecount`,`data-subject`,pic)
+            VALUES (%s, %s, %s, %s ,%s, %s, %s, %s, %s ,%s, %s, %s, %s, %s);
         """
-        self.cursor.execute(insert_sql, (title,url))
-        print("重要：",title,url)
+        self.cursor.execute(insert_sql, (title,score,star,release,duration,director,actors,region,category,enough,showed,votecount,subject,pic))
         self.conn.commit()
 
         return item
 
     def close_spider(self, spider):
         select_send_sql = """
-                select * from douban_rent WHERE douban_rent.`url` not in (select douban_rent_send.`url` from douban_rent_send);
+            select * from douban_movie,douban_movie_send WHERE douban_movie.`data-subject` not in (select douban_movie_send.`data-subject` from douban_movie_send) ORDER BY `data-score` DESC LIMIT 1;
                 """
         self.cursor.execute(select_send_sql)
         result = self.cursor.fetchall()
 
+        send_subject = result[0][12]
+
         insert_send_sql = """
-                    insert into douban_rent_send(`url`)
-                    VALUES (%s);
+                    insert into douban_movie_send(`data-subject`) 
+                    VALUES (%s); 
                 """
-        for i in range(len(result)):
-            send_subject = result[i][1]
-            self.cursor.execute(insert_send_sql, (send_subject,))
+
+        self.cursor.execute(insert_send_sql, (send_subject,))
         self.conn.commit()
 
         self.cursor.close()
